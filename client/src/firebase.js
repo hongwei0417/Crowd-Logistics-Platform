d51ec6ae240257b76e97c7fdc6325a5aded562c7
Hongwei <iamhongwei0417@gmail.com>
2019-10-14 19:07:01 +0800

    snder demo

---
+++ client/src/firebase.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA47p5xt-N-rEkM9mH31nYVl_4uQ276HHo",
+    authDomain: "crowd-logistics-platform.firebaseapp.com",
+    databaseURL: "https://crowd-logistics-platform.firebaseio.com",
+    projectId: "crowd-logistics-platform",
+};
+
+
+export default firebaseConfig
---
+++ client/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import * as firebase from "firebase/app";
+import firebaseConfig from "./firebase"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+firebase.initializeApp(firebaseConfig);
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();

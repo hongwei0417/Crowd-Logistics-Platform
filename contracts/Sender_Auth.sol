9c8a69bc9fbc26bc229d8ddad81108435a616a39
hongwei <iamhongwei0417@gmail.com>
2021-03-29 23:13:51 +0800

    update package

--- contracts/Sender_Auth.sol
+++ contracts/Sender_Auth.sol
@@ -1,80 +1,86 @@
-pragma solidity ^ 0.5 .0;
+pragma solidity ^0.5.0;
+
 // import "github.com/provable-things/ethereum-api/provableAPI.sol";
 
-
 contract Sender_Auth {
-	string name; //姓名
-	uint sex; //生理性別
-	uint birthday; //生日
-	string phone_number; //電話號碼
-	string email; //電子郵件
-	string cid; //身分證字號
-	string psd; //密碼
-	string psd_hint; //密碼提示
+    string name; //姓名
+    uint256 sex; //生理性別
+    uint256 birthday; //生日
+    string phone_number; //電話號碼
+    string email; //電子郵件
+    string cid; //身分證字號
+    string psd; //密碼
+    string psd_hint; //密碼提示
 
-	event verification_status(bool valid);
+    event verification_status(bool valid);
 
+    constructor() public {}
 
-	constructor() public {}
+    function set_sender_data(
+        string memory _name,
+        uint256 _sex,
+        uint256 btd,
+        string memory pn,
+        string memory _email,
+        string memory _cid,
+        string memory _psd,
+        string memory _psd_hint
+    ) public {
+        name = _name;
+        sex = _sex;
+        birthday = btd;
+        phone_number = pn;
+        email = _email;
+        cid = _cid;
+        psd = _psd;
+        psd_hint = _psd_hint;
+    }
 
-	function set_sender_data(
-		string memory _name, uint _sex, uint btd, string memory pn, string memory _email,
-		string memory _cid, string memory _psd, string memory _psd_hint) public {
+    function verify_info() public {
+        require(bytes(name).length != 0, "name is empty!"); //姓名不為空
+        require(bytes(phone_number).length != 0, "phone number is empty!"); //電話不可為空
 
-		name = _name;
-		sex = _sex;
-		birthday = btd;
-		phone_number = pn;
-		email = _email;
-		cid = _cid;
-		psd = _psd;
-		psd_hint = _psd_hint;
-	}
+        bool email_result = verify_email(email); //驗證信箱規則
+        bool cid_result = verify_cid(cid); //驗證身分證字號
+        bool psd_result = verify_psd(psd, psd_hint); //驗證密碼規則
 
-	function verify_info() public {
+        if (email_result && cid_result && psd_result) {
+            emit verification_status(true); //回傳審核成功
+        } else {
+            emit verification_status(false); //回傳審核失敗
+        }
+    }
 
-		require(bytes(name).length != 0, "name is empty!"); //姓名不為空
-		require(bytes(phone_number).length != 0, "phone number is empty!"); //電話不可為空
+    function verify_cid(string memory _cid) public returns (bool valid) {
+        uint256 len = bytes(_cid).length;
+        if (len != 10) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-		bool email_result = verify_email(email); //驗證信箱規則
-		bool cid_result = verify_cid(cid); //驗證身分證字號
-		bool psd_result = verify_psd(psd, psd_hint); //驗證密碼規則
+    function verify_email(string memory _email) public returns (bool valid) {
+        uint256 len = bytes(_email).length;
+        if (len < 10) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-		if (email_result && cid_result && psd_result) {
-			emit verification_status(true); //回傳審核成功
-		} else {
-			emit verification_status(false); //回傳審核失敗
-		}
-	}
+    function verify_psd(string memory _psd, string memory _psd_hint)
+        public
+        view
+        returns (bool valid)
+    {
+        uint256 len1 = bytes(_psd).length;
+        uint256 len2 = bytes(_psd_hint).length;
 
-	function verify_cid(string memory _cid) public returns(bool valid) {
-		uint len = bytes(_cid).length;
-		if (len != 10) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-
-	function verify_email(string memory _email) public returns(bool valid) {
-		uint len = bytes(_email).length;
-		if (len < 10) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-
-	function verify_psd(string memory _psd, string memory _psd_hint) public view returns(bool valid) {
-		uint len1 = bytes(_psd).length;
-		uint len2 = bytes(_psd_hint).length;
-
-		if (len1 < 8 && len2 == 0) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-
-
+        if (len1 < 8 && len2 == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
--- contracts/Sender_request.sol
+++ contracts/Sender_request.sol
@@ -1,76 +1,75 @@
-pragma solidity ^ 0.5 .0;
-
+pragma solidity ^0.5.0;
 
 contract Sender_Request {
+    mapping(address => mapping(uint256 => Order)) User;
 
-  mapping(address => mapping(uint => Order)) User;
+    event return_tx_time(uint256 time);
 
-  event return_tx_time(uint time);
+    struct Order {
+        uint256 delivery_time; //寄送時間
+        uint256 service; //選擇服務
+        bool isUrgent; //急件是否
+        uint256 boxSize; //貨品大小
+        string delivery_start_location; //送貨地點
+        string delivery_end_location; //送貨終點
+        string recipient_name; //收件人姓名
+        string recipient_contact; //收件人聯絡方式
+    }
 
-  struct Order {
-    uint delivery_time; //寄送時間
-    uint service; //選擇服務
-    bool isUrgent; //急件是否
-    uint boxSize; //貨品大小
-    string delivery_start_location; //送貨地點
-    string delivery_end_location; //送貨終點
-    string recipient_name; //收件人姓名
-    string recipient_contact; //收件人聯絡方式
-  }
+    function set_order_info(
+        uint256 dt,
+        string memory dsl,
+        string memory del,
+        string memory rn,
+        string memory rc,
+        uint256 sv,
+        bool isUr,
+        uint256 bs
+    ) public {
+        Order memory order =
+            Order({
+                delivery_time: dt,
+                delivery_start_location: dsl,
+                delivery_end_location: del,
+                recipient_name: rn,
+                recipient_contact: rc,
+                service: sv,
+                isUrgent: isUr,
+                boxSize: bs
+            });
 
+        uint256 time = now;
 
-  function set_order_info(
-    uint dt,
-    string memory dsl,
-    string memory del,
-    string memory rn,
-    string memory rc,
-    uint sv,
-    bool isUr,
-    uint bs
-  ) public {
+        User[msg.sender][time] = order;
 
-    Order memory order = Order({
-      delivery_time: dt,
-      delivery_start_location: dsl,
-      delivery_end_location: del,
-      recipient_name: rn,
-      recipient_contact: rc,
-      service: sv,
-      isUrgent: isUr,
-      boxSize: bs
-    });
+        emit return_tx_time(time);
+    }
 
-    uint time = now;
+    function get_order_info(uint256 time)
+        public
+        view
+        returns (
+            uint256,
+            string memory,
+            string memory,
+            string memory,
+            string memory,
+            uint256,
+            bool,
+            uint256
+        )
+    {
+        Order memory order = User[msg.sender][time];
 
-    User[msg.sender][time] = order;
-
-    emit return_tx_time(time);
-  }
-
-  function get_order_info(uint time) public view returns(
-    uint,
-    string memory,
-    string memory,
-    string memory,
-    string memory,
-    uint,
-    bool,
-    uint
-  ) {
-
-    Order memory order = User[msg.sender][time];
-
-    return (
-      order.delivery_time,
-      order.delivery_start_location,
-      order.delivery_end_location,
-      order.recipient_name,
-      order.recipient_contact,
-      order.service,
-      order.isUrgent,
-      order.boxSize
-    );
-  }
-
+        return (
+            order.delivery_time,
+            order.delivery_start_location,
+            order.delivery_end_location,
+            order.recipient_name,
+            order.recipient_contact,
+            order.service,
+            order.isUrgent,
+            order.boxSize
+        );
+    }
 }
